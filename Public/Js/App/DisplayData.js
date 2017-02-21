62ea6699b36c66652ee20d4262298de2b985e625
koshacool <roman.kushytskyy@gmail.com>
2017-02-21 23:25:58 +0200

    save changes

--- Public/Js/App/DisplayData.js
+++ Public/Js/App/DisplayData.js
@@ -1,95 +1,91 @@
 ;
-define(['sort'], function(sort) {
-	var myData = function () {
-		var infoBlock = document.createElement('div');
-		infoBlock.className = 'message';
-		infoBlock.id = 'message';
+define(['sort'], function (sort) {
+    var myData = function () {
+        var infoBlock = document.createElement('div');
+        infoBlock.className = 'content';
+        infoBlock.id = 'content';
 
-		var title = document.createElement('h1');
-		title.className = 'title';
-		title.innerHTML = 'About me!';
-		infoBlock.appendChild(title);
+        var title = document.createElement('h1');
+        title.className = 'title';
+        title.innerHTML = 'About me!';
+        infoBlock.appendChild(title);
 
-		var h1 = document.createElement('h3');
-		h1.className = 'title';
-		title.innerHTML = 'Roman Kushytskyy';
-		infoBlock.appendChild(h1);
+        var h1 = document.createElement('h3');
+        h1.className = 'title';
+        title.innerHTML = 'Roman Kushytskyy';
+        infoBlock.appendChild(h1);
 
-		var info = document.createElement('p');
-		info.className = 'info';
-		info.innerHTML = 'My name Roman. I very like programming. I already studied such languages: <br>';
-		infoBlock.appendChild(info);
+        var info = document.createElement('p');
+        info.className = 'info';
+        info.innerHTML = 'My name Roman. I very like programming. I already studied such languages: <br>';
+        infoBlock.appendChild(info);
 
 
-		var list = document.createElement('ul');
+        var list = document.createElement('ul');
 
-		var li1 = document.createElement('li');
-		li1.innerHTML = 'PHP';
-		list.appendChild(li1);
+        var li1 = document.createElement('li');
+        li1.innerHTML = 'PHP';
+        list.appendChild(li1);
 
-		var li2 = document.createElement('li');
-		li2.innerHTML = 'HTML';
-		list.appendChild(li2);
+        var li2 = document.createElement('li');
+        li2.innerHTML = 'HTML';
+        list.appendChild(li2);
 
-		var li3 = document.createElement('li');
-		li3.innerHTML = 'JavaSctipt';
-		list.appendChild(li3);		
+        var li3 = document.createElement('li');
+        li3.innerHTML = 'JavaSctipt';
+        list.appendChild(li3);
 
-		infoBlock.appendChild(list);
+        infoBlock.appendChild(list);
 
-		return infoBlock;
+        return infoBlock;
 
-	};
+    };
 
-	var algorithmsButton = function () {
-		var buttonsBLock = document.createElement('div');
-		buttonsBLock.className = 'message';
-		buttonsBLock.id = 'message';
+    var algorithmsButton = function () {
+        var buttonsBLock = document.createElement('div');
+        buttonsBLock.className = 'content';
+        buttonsBLock.id = 'content';
 
-		for (var key in sort) {
-			var button = document.createElement('div');
-			button.className = 'linkButton';
-			button.id = key;
+        for (var key in sort) {
+            var button = document.createElement('div');
+            button.className = 'linkButton';
+            button.id = key;
 
-			var link = document.createElement('a');
-			link.setAttribute('href', '#');
-			link.innerHTML = key;
-			button.appendChild(link);
+            var link = document.createElement('a');
+            link.setAttribute('href', '#');
+            link.innerHTML = key;
+            button.appendChild(link);
 
-			button.addEventListener('click', function (event) {
-				var array = eval('sort.' + key + '()');
-				showSortedArray(array);
-			});
+            button.addEventListener('click', function (event) {
+                var array = eval('sort.' + key + '()');
+                showSortedArray(array);
+            });
 
-			buttonsBLock.appendChild(button);
-		}
-		return buttonsBLock;
-	};
+            buttonsBLock.appendChild(button);
+        }
+        return buttonsBLock;
+    };
 
-	var showSortedArray = function (array) {
+    var showSortedArray = function (array) {
         var div = document.createElement('div');
         div.className = 'linkButton';
         div.innerHTML = array;
 
-        document.getElementById('message').appendChild(div);
+        document.getElementById('content').appendChild(div);
     };
 
-	return {		
-		aboutMe: function () { 			
-			var infoBlock = document.getElementById('infoBlock');
-			infoBlock.replaceChild(myData(), document.getElementById('message'));
-			
-		},
+    return {
+        aboutMe: function () {
+            var infoBlock = document.getElementById('contentBlock');
+            infoBlock.replaceChild(myData(), document.getElementById('content'));
+        },
 
-		algorithms: function () { 			
-			var infoBlock = document.getElementById('infoBlock');
-			infoBlock.replaceChild(algorithmsButton(), document.getElementById('message'));
-			
-		},
+        algorithms: function () {
+            var infoBlock = document.getElementById('contentBlock');
+            infoBlock.replaceChild(algorithmsButton(), document.getElementById('content'));
+        },
 
 
-
-
-	}
+    }
 });
 
